7a9363854e6c0405c94626d5ebae1180d3f01043
Your Name <your_email@example.com>
2026-08-22 22:47:48 +0530

    First And Last position

---
+++ BinarySearch/FirstAndLast.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+     int findBound(vector<int>& nums, int target, bool isFirst) {
+        int s=0;
+        int e=nums.size()-1;
+        int ans=-1;
+        while(s<=e){
+           int mid=s+(e-s)/2;
+           if(nums[mid]==target){
+               ans=mid;  
+               if (isFirst){
+                e=mid-1;
+               }else{
+                s=mid+1;
+               }
+            } 
+            else if(nums[mid]<target){
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+    
+        return ans;
+    }
+        vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return {first, last};
+    }
+};
