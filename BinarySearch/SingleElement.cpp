bdff5bd9bcaaf768ef7f99ce1a790720c1ee2f23
Your Name <your_email@example.com>
2026-08-24 23:24:54 +0530

    Single Element in a sorted array

---
+++ BinarySearch/SingleElement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // Ensure mid is even for easy pair comparison
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            
+            // If pair is valid, target is on the right
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return nums[left];
+    }
+};
